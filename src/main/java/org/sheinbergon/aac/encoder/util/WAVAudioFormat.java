1780a9418173152feb4c94e891eae6260d3e2e13
Idan Sheinberg <ishinberg0@gmail.com>
2020-10-02 19:41:15 +0300

    Linting fixes

--- src/main/java/org/sheinbergon/aac/encoder/util/WAVAudioFormat.java
+++ src/main/java/org/sheinbergon/aac/encoder/util/WAVAudioFormat.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
  * WAV header AudioFormat 16 bit value.
  * <p>
  * ONLY PCM is supported in Fdk-AAC. Other values such as
+ * <ul>
  * <li>ADPCM((short) 2) </li>
  * <li>IEEE_FLOAT((short) 3) </li>
  * <li>ALAW((short) 6) </li>
+ * </ul>
  * are considered invalid by the Fdk-AAC library
  */
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
